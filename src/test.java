a29bced8e53727b440a5dd7b88caba40d3507bf7
Hawkeye Parker <parker.hawkeye@gmail.com>
2017-10-06 19:51:25 -0700

    master: go again and again (second change)

--- src/test.java
+++ src/test.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("TestRepo go!");
         System.out.println("TestRepo go again!");
+        System.out.println("TestRepo go again and again!");
     }
 }
