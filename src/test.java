5637254397947012c8b529c7a2e4159744512a45
Hawkeye Parker <parker.hawkeye@gmail.com>
2017-10-12 15:03:03 -0700

    - mast rebase double 1

--- src/test.java
+++ src/test.java
@@ -3,7 +3,6 @@ package TestRepo;
 public class Main {
     public static void main(String[] args) {
         System.out.println("TestRepo go!");
-        System.out.println("master rebase new one");
-        System.out.println("exp rebase new 2");
+        System.out.println("mast rebase double 1");
     }
 }
