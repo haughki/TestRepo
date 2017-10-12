1dda282b6fe3d4206f0f1f7d4391b22ce31a537d
Hawkeye Parker <parker.hawkeye@gmail.com>
2017-10-12 15:03:19 -0700

    - mast rebase double 2

--- src/test.java
+++ src/test.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("TestRepo go!");
         System.out.println("mast rebase double 1");
+        System.out.println("mast rebase double 2");
     }
 }
