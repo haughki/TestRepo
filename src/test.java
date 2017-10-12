808f61087f33f7af5af2be6c4bbd70a83135b836
Hawkeye Parker <parker.hawkeye@gmail.com>
2017-10-12 14:39:12 -0700

    - master reb new 1

--- src/test.java
+++ src/test.java
@@ -3,6 +3,6 @@ package TestRepo;
 public class Main {
     public static void main(String[] args) {
         System.out.println("TestRepo go!");
-        System.out.println("master and exper rebase 3 prime");
+        System.out.println("master rebase new one");
     }
 }
