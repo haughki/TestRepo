4ac347fef3a0c8e299a155ae15fe5c71816fc0a8
Hawkeye Parker <parker.hawkeye@gmail.com>
2017-10-12 14:39:33 -0700

    - master reb new 2

--- src/test.java
+++ src/test.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("TestRepo go!");
         System.out.println("master rebase new one");
+        System.out.println("master rebase new two");
     }
 }
