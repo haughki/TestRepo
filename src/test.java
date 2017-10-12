f3e8b996b39e234854e2739b439b1cfd7381ef07
Hawkeye Parker <parker.hawkeye@gmail.com>
2017-10-12 16:35:24 -0700

    - mast delete stuff

--- src/test.java
+++ src/test.java
@@ -3,8 +3,5 @@ package TestRepo;
 public class Main {
     public static void main(String[] args) {
         System.out.println("TestRepo go!");
-        System.out.println("mast rebase double 1");
-        System.out.println("mast rebase double 2");
-        System.out.println("temp repo change");
     }
 }
