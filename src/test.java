8375baa501d4881facdd67aa4026bcc4002439bd
Hawkeye Parker <parker.hawkeye@gmail.com>
2017-10-12 14:29:43 -0700

    - exp reb 3 conflict

--- src/test.java
+++ src/test.java
@@ -3,6 +3,6 @@ package TestRepo;
 public class Main {
     public static void main(String[] args) {
         System.out.println("TestRepo go!");
-        System.out.println("master rebase 3 conflict");
+        System.out.println("master and exper rebase 3 prime");
     }
 }
