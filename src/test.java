3b9ce00b7665527927d186cd4f5bb2d7774959a9
Hawkeye Parker <parker.hawkeye@gmail.com>
2017-10-12 14:20:55 -0700

    - mast reb two

--- src/test.java
+++ src/test.java
@@ -3,6 +3,6 @@ package TestRepo;
 public class Main {
     public static void main(String[] args) {
         System.out.println("TestRepo go!");
-        System.out.println("master rebase one");
+        System.out.println("master rebase two");
     }
 }
