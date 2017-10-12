3802d8265d195df48850f0406fd24bb8c9c396ac
Hawkeye Parker <parker.hawkeye@gmail.com>
2017-10-12 14:30:06 -0700

    - master reb 3 conflict

--- src/test.java
+++ src/test.java
@@ -3,6 +3,6 @@ package TestRepo;
 public class Main {
     public static void main(String[] args) {
         System.out.println("TestRepo go!");
-        System.out.println("master rebase two");
+        System.out.println("master rebase 3 conflict");
     }
 }
